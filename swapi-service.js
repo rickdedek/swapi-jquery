326418ec18925a64b16973a276d3e8f2f6e80351
Rick Dedek <ricardo.dedek@gmail.com>
2022-05-29 13:30:48 -0300

    feat: update-version

--- swapi-service.js
+++ swapi-service.js
@@ -1,1 +1,20 @@
-console.log($(".btn-get-people"));
+// Funcion que se ejecuta la presionar el boton
+function getPeople() {
+    //   const name = "Lukeeskaiworker";
+    //   changePeopleName(name);
+    const number = $("#input-number").val();
+    $.ajax({
+      url: "https://swapi.dev/api/people/" + number,
+      success: (result) => {
+        // ARROW FUNCTION / funcion sin nombre (param) => { return 0 }
+        changePeopleName(result.name);
+      },
+      error: () => {
+        alert("People not found");
+      },
+    });
+  }
+  // Funcion que hace el cambio de nombre en el span
+  function changePeopleName(name) {
+    $("#person-name").html(name);
+  }
